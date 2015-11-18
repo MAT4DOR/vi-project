6de263da043e2a4fd395e7453653ae24a6e3f98a
André Baltazar <me@andrebaltazar.com>
2015-11-18 02:40:42 +0000

    start distribution plots

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 var full_dataset;
+var visualizations = {};
+var countryColors = {1: '#f50', 2:'#0f5'};
+var attributeColors = {1: '#44f', 2:'#8f4'};
 
 var dsv = d3.dsv(';', 'text/plain');
 dsv('gender_inequality.csv', function (data) {
@@ -23,11 +26,123 @@ function initializeInterface() {
         countrySelectionOne.append('<option value="' + countries[i].id + '">' + countries[i].country + '</option>');
         countrySelectionTwo.append('<option value="' + countries[i].id + '">' + countries[i].country + '</option>');
     }
+    countrySelectionOne.css('background-color', countryColors[1]);
+    countrySelectionTwo.css('background-color', countryColors[2]);
 }
 
 function initializeVisualizations() {
-
+    initTask2();
 }
+
+function initTask2() {
+    var options = {
+        width: 1000,
+        height: 400,
+        padding: 20
+    }
+    var svg = d3.select('#task2').append('svg');
+    svg.attr('width', options.width).attr('height', options.height);
+
+    var attributes = [
+        {name: 'gii_value', max:1},
+        {name: 'mmr', max:-1},
+        {name: 'abr', max:-1},
+        {name: 'ssp', max:100},
+        {name: 'edu_female', max:100},
+        {name: 'edu_male', max:100},
+        {name: 'edu_diff', max:100},
+        {name: 'labour_female', max:100},
+        {name: 'labour_male', max:100},
+        {name: 'labour_diff', max:100},
+    ];
+    var enterSelection = svg.selectAll('circle').data(full_dataset).enter();
+
+    var scale = {};
+    for (var attrIndex = 0; attrIndex < attributes.length; ++attrIndex) {
+        var attr = attributes[attrIndex].name;
+        var max = attributes[attrIndex].max;
+        if (max == -1) {
+            for (var i = 0; i < full_dataset.length; ++i) {
+                if (Math.abs(full_dataset[i][attr]) > max)
+                    max = Math.abs(full_dataset[i][attr]);
+            }
+        }
+        attributes[attrIndex].max = max;
+        scale[attr] = d3.scale.linear().domain([max, attr.endsWith('_diff') ? -max : 0]).range([options.padding,options.height-options.padding]);
+
+        enterSelection.append('circle').attr('class', 'to-remove').filter(function (d) { return d[attr] != -1; })
+            .attr('fill', '#555')
+            .attr('r', 5)
+            .attr('cx', 40 * attrIndex + options.padding)
+            .attr('cy', function(d, i) { return scale[attr](parseFloat(d[attr].replace(/,/g, '.'))); })
+            .attr('attr', attr)
+            .attr('country', function(d, i) { return i; })
+            .attr('opacity', 0.12)
+            .attr('class', '');
+    }
+    svg.selectAll('circle.to-remove').remove();
+    visualizations.task2 = { options: options, attributes: attributes, svg: svg, scale: scale };
+    initSelectorMarksTask2(1);
+    initSelectorMarksTask2(2);
+    updateTask2(1, $('#country-selection-one option:selected').attr('value'))
+    updateTask2(2, $('#country-selection-two option:selected').attr('value'))
+}
+
+function initSelectorMarksTask2(countrySelectorNumber) {
+    var options = visualizations.task2.options;
+    var attributes = visualizations.task2.attributes;
+    var svg = visualizations.task2.svg;
+
+    var group = svg.append('g').attr('class', 'selector-' + countrySelectorNumber);
+    for (var attrIndex = 0; attrIndex < attributes.length; ++attrIndex) {
+        var attr = attributes[attrIndex].name;
+        group.insert('circle')
+            .attr('fill', countryColors[countrySelectorNumber])
+            .attr('r', 5)
+            .attr('cx', 40 * attrIndex + options.padding)
+            .attr('cy', -10000)
+            .attr('opacity', 0.2)
+            .attr('attr', attr);
+    }
+}
+
+function updateTask2(countrySelectorNumber, selectedCountry) {
+    var options = visualizations.task2.options;
+    var attributes = visualizations.task2.attributes;
+    var svg = visualizations.task2.svg;
+    var scale = visualizations.task2.scale;
+    var selection = svg.selectAll('g.selector-' + countrySelectorNumber).data([full_dataset[selectedCountry]]);
+    for (var attrIndex = 0; attrIndex < attributes.length; ++attrIndex) {
+        var attr = attributes[attrIndex].name;
+        selection.select('circle[attr="' + attr + '"]')
+            .attr('fill', countryColors[countrySelectorNumber])
+            .attr('r', 5)
+            .attr('cx', 40 * attrIndex + options.padding)
+            .attr('cy', function (d, i) {
+                if (d[attr] == '-1')
+                    return -100000;
+                if (attr.endsWith('_diff')) {
+                    var attrParent = attr.substr(0, attr.indexOf('_diff'));
+                    if (d[attrParent + '_male'] == '-1' || d[attr + '_female'] == '-1')
+                        return -100000;
+                }
+                return scale[attr](parseFloat(d[attr].replace(/,/g, '.')));
+            })
+            .attr('opacity', 0.6);
+    }
+}
+
+function changeSelectedCountry(countrySelectorNumber, selectedCountry) {
+    updateTask2(countrySelectorNumber, selectedCountry);
+}
+
+$('#country-selection-one').change(function() {
+    changeSelectedCountry(1, $(this).find('option:selected').attr('value'));
+});
+
+$('#country-selection-two').change(function() {
+    changeSelectedCountry(2, $(this).find('option:selected').attr('value'));
+});
 
 function changeVisualizations() {
     var value = $('.selected').attr("data-id");
@@ -40,7 +155,7 @@ function changeVisualizations() {
 function sideWaysDivergentBar() {
     var w = 800;
     var h = 5000;
-    var svg = d3.select("#charts");
+    var svg = d3.select("#task4");
     svg = svg.append("svg")
             .attr("width",w)
             .attr("height",h); 
@@ -119,12 +234,6 @@ $('#attribute-selection-one li').click(function() {
     self.parent().children('.selected').removeClass('selected');
     self.addClass('selected');
     changeVisualizations();
-});
-
-$('#tst').click(function(){
-    var value = $('.selected').text();
-    value = value || 'No row Selected';
-    alert(value);
 });
 
 function getSelectedText(elementId) {
