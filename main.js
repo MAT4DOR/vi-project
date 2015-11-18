00ebecbb00eb684cc7c07d9e62e36aa11f265fc8
André Baltazar <me@andrebaltazar.com>
2015-11-18 19:40:15 +0000

    finish task2

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
 var full_dataset;
 var visualizations = {};
-var countryColors = {1: '#f50', 2:'#0f5'};
+var countryColors = ['#f50', '#0f5'];
 var attributeColors = {1: '#44f', 2:'#8f4'};
+var attributes = [
+    {col: 'gii_value', shortname:'GII Value', max:1},
+    {col: 'mmr', shortname:'Maternal Mortality', max:-1},
+    {col: 'abr', shortname:'Adolencent Births', max:-1},
+    {col: 'ssp', shortname:'Seats Parliament', max:100},
+    {col: 'edu_female', shortname:'Education Female', max:100},
+    {col: 'edu_male', shortname:'Education Male', max:100},
+    {col: 'edu_diff', shortname:'Education Difference', max:100},
+    {col: 'labour_female', shortname:'Labour Female', max:100},
+    {col: 'labour_male', shortname:'Labour Male', max:100},
+    {col: 'labour_diff', shortname:'Labour Difference', max:100},
+];
 
 var dsv = d3.dsv(';', 'text/plain');
 dsv('gender_inequality.csv', function (data) {
@@ -9,6 +21,20 @@ dsv('gender_inequality.csv', function (data) {
     console.log(data);
     initialize();
 });
+
+function initializeData() {
+    for (var attrIndex = 0; attrIndex < attributes.length; ++attrIndex) {
+        var attr = attributes[attrIndex].col;
+        var max = attributes[attrIndex].max;
+        if (max == -1) {
+            for (var i = 0; i < full_dataset.length; ++i) {
+                if (Math.abs(full_dataset[i][attr]) > max)
+                    max = Math.abs(full_dataset[i][attr]);
+            }
+        }
+        attributes[attrIndex].max = max;
+    }
+}
 
 function initializeInterface() {
     var countries = [];
@@ -20,14 +46,23 @@ function initializeInterface() {
         return a.country.localeCompare(b.country);
     });
 
-    var countrySelectionOne = $('#country-selection-one');
-    var countrySelectionTwo = $('#country-selection-two');
-    for(var i = 0; i < countries.length; ++i) {
-        countrySelectionOne.append('<option value="' + countries[i].id + '">' + countries[i].country + '</option>');
-        countrySelectionTwo.append('<option value="' + countries[i].id + '">' + countries[i].country + '</option>');
+    var countrySelection = $('#country-selection');
+    for (var n = 0; n < countryColors.length; ++n) {
+        countrySelection.append('<select id="country-selection-' + n + '" name="country"></select>');
     }
-    countrySelectionOne.css('background-color', countryColors[1]);
-    countrySelectionTwo.css('background-color', countryColors[2]);
+
+    for (var n = 0; n < countryColors.length; ++n) {
+        var countrySelector = $('#country-selection-' + n);
+        for (var i = 0; i < countries.length; ++i) {
+            countrySelector.append('<option value="' + countries[i].id + '">' + countries[i].country + '</option>');
+        }
+        countrySelector.css('background-color', countryColors[n]);
+        (function (n) {
+            countrySelector.change(function () {
+                changeSelectedCountry(n, $(this).find('option:selected').attr('value'));
+            });
+        })(n);
+    }
 }
 
 function initializeVisualizations() {
@@ -38,71 +73,75 @@ function initTask2() {
     var options = {
         width: 1000,
         height: 400,
-        padding: 20
+        padding: 60,
+        paddingLeft: 50,
+        distanceBetween: 100,
     }
     var svg = d3.select('#task2').append('svg');
     svg.attr('width', options.width).attr('height', options.height);
 
-    var attributes = [
-        {name: 'gii_value', max:1},
-        {name: 'mmr', max:-1},
-        {name: 'abr', max:-1},
-        {name: 'ssp', max:100},
-        {name: 'edu_female', max:100},
-        {name: 'edu_male', max:100},
-        {name: 'edu_diff', max:100},
-        {name: 'labour_female', max:100},
-        {name: 'labour_male', max:100},
-        {name: 'labour_diff', max:100},
-    ];
     var enterSelection = svg.selectAll('circle').data(full_dataset).enter();
 
-    var scale = {};
+    var scaleX = d3.scale.ordinal().rangeRoundBands([0, options.width]);
+    scaleX.domain(attributes.map(function(d) { return d.shortname; }));
+    var xAxis = d3.svg.axis().scale(scaleX).orient('bottom');
+    svg.append('g')
+        .attr('class', 'axis task2')
+        .attr('transform', 'translate(0,' + (options.height-options.padding+10) + ')')
+        .call(xAxis)
+        .selectAll('.tick text')
+        .call(wrap, scaleX.rangeBand());
+
+    var scaleY = {};
     for (var attrIndex = 0; attrIndex < attributes.length; ++attrIndex) {
-        var attr = attributes[attrIndex].name;
+        var attr = attributes[attrIndex].col;
         var max = attributes[attrIndex].max;
-        if (max == -1) {
-            for (var i = 0; i < full_dataset.length; ++i) {
-                if (Math.abs(full_dataset[i][attr]) > max)
-                    max = Math.abs(full_dataset[i][attr]);
-            }
-        }
-        attributes[attrIndex].max = max;
-        scale[attr] = d3.scale.linear().domain([max, attr.endsWith('_diff') ? -max : 0]).range([options.padding,options.height-options.padding]);
+        var min = attr.endsWith('_diff') ? -max : 0;
+        scaleY[attr] = d3.scale.linear().domain([max, min]).range([options.padding,options.height-options.padding]);
+
+        var axisGroup = svg.append('g').attr('class', 'axis-' + attr);
+        var x0 = options.distanceBetween * attrIndex + options.paddingLeft;
+        var yAxis = d3.svg.axis().scale(scaleY[attr]).orient("left").ticks(0, "f").tickValues([min, max]);
+        axisGroup.append("g")
+            .attr('transform','translate(' + (x0-8-0.5) + ', -0.5)')
+            .attr('class', 'axis task2')
+            .call(yAxis);
 
         enterSelection.append('circle').attr('class', 'to-remove').filter(function (d) { return d[attr] != -1; })
             .attr('fill', '#555')
             .attr('r', 5)
-            .attr('cx', 40 * attrIndex + options.padding)
-            .attr('cy', function(d, i) { return scale[attr](parseFloat(d[attr].replace(/,/g, '.'))); })
+            .attr('cx', options.distanceBetween * attrIndex + options.paddingLeft)
+            .attr('cy', function(d, i) { return scaleY[attr](parseFloat(d[attr].replace(/,/g, '.'))); })
             .attr('attr', attr)
             .attr('country', function(d, i) { return i; })
             .attr('opacity', 0.12)
             .attr('class', '');
     }
+
     svg.selectAll('circle.to-remove').remove();
-    visualizations.task2 = { options: options, attributes: attributes, svg: svg, scale: scale };
-    initSelectorMarksTask2(1);
-    initSelectorMarksTask2(2);
-    updateTask2(1, $('#country-selection-one option:selected').attr('value'))
-    updateTask2(2, $('#country-selection-two option:selected').attr('value'))
-}
+    visualizations.task2 = { options: options, attributes: attributes, svg: svg, scaleY: scaleY };
+    for (var countrySelectorNumber = 0; countrySelectorNumber < countryColors.length; ++countrySelectorNumber) {
+        var group = svg.append('g').attr('class', 'selector-' + countrySelectorNumber);
+        for (var attrIndex = 0; attrIndex < attributes.length; ++attrIndex) {
+            var attr = attributes[attrIndex].col;
+            group.append('circle')
+                .attr('fill', countryColors[countrySelectorNumber])
+                .attr('r', 5)
+                .attr('cx', options.distanceBetween * attrIndex + options.paddingLeft)
+                .attr('cy', -10000)
+                .attr('opacity', 0.6)
+                .attr('attr', attr);
 
-function initSelectorMarksTask2(countrySelectorNumber) {
-    var options = visualizations.task2.options;
-    var attributes = visualizations.task2.attributes;
-    var svg = visualizations.task2.svg;
-
-    var group = svg.append('g').attr('class', 'selector-' + countrySelectorNumber);
-    for (var attrIndex = 0; attrIndex < attributes.length; ++attrIndex) {
-        var attr = attributes[attrIndex].name;
-        group.insert('circle')
-            .attr('fill', countryColors[countrySelectorNumber])
-            .attr('r', 5)
-            .attr('cx', 40 * attrIndex + options.padding)
-            .attr('cy', -10000)
-            .attr('opacity', 0.2)
-            .attr('attr', attr);
+            var x0 = options.distanceBetween * attrIndex + options.paddingLeft;
+            group.append('line')
+                .attr('x1', x0 - 2).attr('x2', x0 - 8)
+                .attr('y1', -10000).attr('y2', -10000)
+                .attr('stroke-width', 1).attr('stroke', 'black').attr('attr', attr);
+            group.append('text').attr('x', x0 - 10 - 0.5).attr('y', -10000)
+                .attr('class', 'axistext').attr('dy', '.32em')
+                .attr('fill', 'black').attr('text-anchor', 'end').text('').attr('attr', attr);
+        }
+        updateTask2(countrySelectorNumber, $('#country-selection-' + countrySelectorNumber + ' option:selected').attr('value'))
     }
 }
 
@@ -110,25 +149,33 @@ function updateTask2(countrySelectorNumber, selectedCountry) {
     var options = visualizations.task2.options;
     var attributes = visualizations.task2.attributes;
     var svg = visualizations.task2.svg;
-    var scale = visualizations.task2.scale;
+    var scaleY = visualizations.task2.scaleY;
     var selection = svg.selectAll('g.selector-' + countrySelectorNumber).data([full_dataset[selectedCountry]]);
     for (var attrIndex = 0; attrIndex < attributes.length; ++attrIndex) {
-        var attr = attributes[attrIndex].name;
-        selection.select('circle[attr="' + attr + '"]')
-            .attr('fill', countryColors[countrySelectorNumber])
-            .attr('r', 5)
-            .attr('cx', 40 * attrIndex + options.padding)
-            .attr('cy', function (d, i) {
-                if (d[attr] == '-1')
+        var attr = attributes[attrIndex].col;
+        var centerY = function (d, i) {
+            if (d[attr] == '-1')
+                return -100000;
+            if (attr.endsWith('_diff')) {
+                var attrParent = attr.substr(0, attr.indexOf('_diff'));
+                if (d[attrParent + '_male'] == '-1' || d[attr + '_female'] == '-1')
                     return -100000;
-                if (attr.endsWith('_diff')) {
-                    var attrParent = attr.substr(0, attr.indexOf('_diff'));
-                    if (d[attrParent + '_male'] == '-1' || d[attr + '_female'] == '-1')
-                        return -100000;
-                }
-                return scale[attr](parseFloat(d[attr].replace(/,/g, '.')));
-            })
-            .attr('opacity', 0.6);
+            }
+            return scaleY[attr](parseFloat(d[attr].replace(/,/g, '.')));
+        };
+        selection.select('circle[attr="' + attr + '"]').attr('cy', centerY);
+        selection.select('text[attr="' + attr + '"]').attr('y',  centerY).text(function(d) { return Math.round(parseFloat(d[attr].replace(/,/g, '.'))*10)/10; });
+        var y = function (d, i) {
+            if (d[attr] == '-1')
+                return -100000;
+            if (attr.endsWith('_diff')) {
+                var attrParent = attr.substr(0, attr.indexOf('_diff'));
+                if (d[attrParent + '_male'] == '-1' || d[attr + '_female'] == '-1')
+                    return -100000;
+            }
+            return parseInt(scaleY[attr](parseFloat(d[attr].replace(/,/g, '.')))) + 0.5;
+        };
+        selection.select('line[attr="' + attr + '"]').attr('y1', y).attr('y2',  y);
     }
 }
 
@@ -136,13 +183,30 @@ function changeSelectedCountry(countrySelectorNumber, selectedCountry) {
     updateTask2(countrySelectorNumber, selectedCountry);
 }
 
-$('#country-selection-one').change(function() {
-    changeSelectedCountry(1, $(this).find('option:selected').attr('value'));
-});
-
-$('#country-selection-two').change(function() {
-    changeSelectedCountry(2, $(this).find('option:selected').attr('value'));
-});
+// from: http://bl.ocks.org/mbostock/7555321
+function wrap(text, width) {
+    text.each(function() {
+        var text = d3.select(this),
+            words = text.text().split(/\s+/).reverse(),
+            word,
+            line = [],
+            lineNumber = 0,
+            lineHeight = 1.1, // ems
+            y = text.attr("y"),
+            dy = parseFloat(text.attr("dy")),
+            tspan = text.text(null).append("tspan").attr("x", 0).attr("y", y).attr("dy", dy + "em");
+        while (word = words.pop()) {
+            line.push(word);
+            tspan.text(line.join(" "));
+            if (tspan.node().getComputedTextLength() > width) {
+                line.pop();
+                tspan.text(line.join(" "));
+                line = [word];
+                tspan = text.append("tspan").attr("x", 0).attr("y", y).attr("dy", ++lineNumber * lineHeight + dy + "em").text(word);
+            }
+        }
+    });
+}
 
 function changeVisualizations() {
     var value = $('.selected').attr("data-id");
@@ -158,11 +222,11 @@ function sideWaysDivergentBar() {
     var svg = d3.select("#task4");
     svg = svg.append("svg")
             .attr("width",w)
-            .attr("height",h); 
+            .attr("height",h);
 
     var comparingFunction = function compareNumbers(a, b) {
         return parseInt(b.labour_diff) - parseInt(a.labour_diff);
-        };     
+        };
 
     var orderedDataset = full_dataset;
     orderedDataset = orderedDataset.sort(comparingFunction);
@@ -193,8 +257,8 @@ function task1() {
     var h = 400;
     var svg = d3.select("#task1");
 
-    var country1 = getSelectedText("country-selection-one");
-    var country2 = getSelectedText("country-selection-two");
+    var country1 = getSelectedText("country-selection-0");
+    var country2 = getSelectedText("country-selection-1");
     var data1;
     var data2;
     var selectedAttr = $('.selected').attr("data-id");
@@ -225,6 +289,7 @@ function task1() {
 }
 
 function initialize() {
+    initializeData();
     initializeInterface();
     initializeVisualizations();
 }
